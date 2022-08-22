66b2b9470d98669d053159b9567fea8fae1a164b
Brian Denmark <testingarea479@gmail.com>
2022-08-21 21:22:55 -0700

    implemented routs for ninja and sensei bonuses

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/checkerboard/<int:x>')
+def singlerow(x):
+    return render_template('single.html',x=x,color1='black',color2='red')
+
+@app.route('/checkerboard/<int:x>/<int:y>')
+def doublerow(x,y):
+    return render_template('double.html',x=x,y=y,color1='black',color2='red')
+
 @app.route('/checkerboard/<int:x>/<int:y>/<string:color1>/<string:color2>')
 def checkerboard(x,y,color1,color2):
     return render_template('index.html',x=x,y=y,color1=color1,color2=color2)
