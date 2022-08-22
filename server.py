066f01eea305ddebc3394199baf2caa1dd70c1e9
Brian Denmark <testingarea479@gmail.com>
2022-08-21 20:52:14 -0700

    added static directory

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/checkerboard/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def checkerboard(x,y,color1,color2):
+    return render_template('index.html',x=x,y=y,color1=color1,color2=color2)
 
 if __name__ == '__main__':
     app.run(debug=True)
